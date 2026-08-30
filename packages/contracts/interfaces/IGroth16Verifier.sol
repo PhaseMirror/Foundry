6e55eb9af20a978466df990b4e3703c51fba5522
PhaseMirror <info@citizengardens.org>
2026-08-29 23:15:45 -0400

    Add ACEVerifier contract and configuration files
    - Introduced ACEVerifier.sol for zero-knowledge proof verification.
    - Added foundry.toml for project configuration.
    - Included pot12_final.ptau for trusted setup data.

--- packages/contracts/interfaces/IGroth16Verifier.sol
+++ packages/contracts/interfaces/IGroth16Verifier.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.24;
 interface IGroth16Verifier {
     function verifyProof(uint256[2] calldata pA,uint256[2][2] calldata pB,uint256[2] calldata pC,uint256[] calldata pubSignals) external view returns (bool);
     function verifyProof(uint256[2] calldata pA,uint256[2][2] calldata pB,uint256[2] calldata pC,uint256[1] calldata pubSignals) external view returns (bool);
+    function verifyProof(uint256[2] calldata pA,uint256[2][2] calldata pB,uint256[2] calldata pC,uint256[2] calldata pubSignals) external view returns (bool);
     function verifyProof(uint256[2] calldata pA,uint256[2][2] calldata pB,uint256[2] calldata pC,uint256[4] calldata pubSignals) external view returns (bool);
     function verifyProof(uint256[2] calldata pA,uint256[2][2] calldata pB,uint256[2] calldata pC,uint256[5] calldata pubSignals) external view returns (bool);
     function verifyProof(uint256[2] calldata pA,uint256[2][2] calldata pB,uint256[2] calldata pC,uint256[6] calldata pubSignals) external view returns (bool);
